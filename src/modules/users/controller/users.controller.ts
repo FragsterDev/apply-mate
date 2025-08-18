6ca456741a6e2b02fdf14be3934c4f4f594f8d64
Pratik Sharma <pratiksharma059@gmail.com>
2025-08-18 13:24:22 +0530

    fix

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import { success, error } from "../../../utils/responses/responses";
 
 export const createUser = (req: Request, res: Response, next: NextFunction) => {
   try {
